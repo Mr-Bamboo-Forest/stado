eed5253c15f09f9a5951d32b8a7e99eb587b00e7
Bamboo Forest <113477194+Mr-Bamboo-Forest@users.noreply.github.com>
2026-05-27 20:12:30 +1000

    app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'inline',
+      manifest: {
+        name: 'Stado',
+        short_name: 'Stado',
+        description: 'Find your game. Show up and play.',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#F1EFE8',
+        theme_color: '#F1EFE8',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
